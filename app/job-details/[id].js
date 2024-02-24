c2666e40abe2929fe35b45f391cf042686bf88c4
João Marcelo Dantas <jmgrd98@gmail.com>
2024-02-24 19:09:48 -0300

    job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -19,7 +19,7 @@ const JobDetails = () => {
     const [refreshing, setRefreshing] = useState(false);
 
     const onRefresh = () => {
-        
+
     }
 
   return (
@@ -48,7 +48,30 @@ const JobDetails = () => {
 
         <>
             <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-                
+                {
+                    isLoading ? (
+                        <ActivityIndicator size='large' color={COLORS.primary}/>
+                    ) : (
+                        error ? (
+                            <Text>Something went wrong.</Text>
+                        ) : data.length === 0 ? (
+                            <Text>No data.</Text>
+                        ) : (
+                            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                                <Company 
+                                    companyLogo={data[0].employer_logo}
+                                    jobTitle={data[0].job_title}
+                                    companyName={data[0].employer_name}
+                                    location={data[0].job_country}
+                                />
+
+                                <JobTabs 
+
+                                />
+                            </View>
+                        )
+                    )
+                }
             </ScrollView>
         </>
     </SafeAreaView>
